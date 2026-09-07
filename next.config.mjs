defaa01ce2e3460123a462f283656db13cdebbf1
Sakshi Rane <sakshirane284@gmail.com>
2026-09-07 23:40:06 +0530

    Configure GitHub Pages automatic deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isGithubActions ? '/for-sakshi' : '',
+  assetPrefix: isGithubActions ? '/for-sakshi/' : '',
 };
 
 export default nextConfig;
